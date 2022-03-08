2484d81669447944b82b9011220b7cab1178f709
Vishal Prajapati <vishal.p@yudiz.in>
2022-03-08 14:01:33 +0530

    Api: Chat Room Listing Response Changed

--- app/Http/Controllers/api/v1/ChatController.php
+++ app/Http/Controllers/api/v1/ChatController.php
@@ -69,7 +69,7 @@ class ChatController extends Controller
                 $user = $request->user();
                 $rooms = ChatRoom::with(['creator:id,custom_id,first_name,last_name,profile_photo',
                                         'participator:id,custom_id,first_name,last_name,profile_photo',
-                                        'latestMessage'])
+                                        'latestMessage.sender:id,custom_id'])
                                 ->whereHas('chatMessages')
                                 ->selectRaw("chat_rooms.*, (SELECT MAX(created_at) from chat_messages WHERE chat_messages.room_id=chat_rooms.id) as latest_message_on")
                                 ->orderBy("latest_message_on", "DESC")
