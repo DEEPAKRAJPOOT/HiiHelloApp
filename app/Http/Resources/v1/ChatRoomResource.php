f20969ddf3a5a4d5444f4835a961f307a115463d
Vishal Prajapati <vishal.p@yudiz.in>
2022-04-29 16:49:46 +0530

    Api: Chat Room List Changes

--- app/Http/Resources/v1/ChatRoomResource.php
+++ app/Http/Resources/v1/ChatRoomResource.php
@@ -18,7 +18,9 @@ class ChatRoomResource extends JsonResource
         return [
             'id'            =>  $this->custom_id,
             'is_active'     =>  $this->is_active ? $this->is_active == 'y' ? true : false : false,
-            'is_blocked'    =>  $this->block_by_count ? $this->block_by_count > 0 ? true : false : false,
+            'block_by'  =>  [
+                'id'            =>  $this->blockBy ? $this->blockBy->custom_id : "",
+            ],
             'creator'  =>  [
                 'id'            =>  $this->creator ? $this->creator->custom_id : "",
                 'full_name'     =>  $this->creator ? $this->creator->full_name : "",
