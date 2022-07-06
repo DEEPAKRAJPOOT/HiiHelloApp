036267261f248bad85bfb730b8d2192714409571
Vishal Prajapati <vishal.p@yudiz.in>
2022-07-06 15:51:14 +0530

    Api: User Subscription Details Api Developed

--- app/Http/Resources/v1/SubscriptionResource.php
+++ app/Http/Resources/v1/SubscriptionResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\v1;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class SubscriptionResource extends JsonResource
 {
@@ -29,11 +30,21 @@ class SubscriptionResource extends JsonResource
 
     public function with($request)
     {
+        $is_subscribed = false;
+        $subscription_end_date = "";
+        if( !Auth::guest() ) {
+            if( Auth::user()->is_subscribed == 'y' && Auth::user()->subscription_end_date >= \Carbon\Carbon::today()->format('Y-m-d') ){
+                $is_subscribed = true;
+            }
+            $subscription_end_date = Auth::user()->subscription_end_date ?? "";
+        }
         return [
             'meta' => [ 
-                'api'               =>  'v.1.0',
-                'url'               =>  url()->current(),
-                'language'          =>  app()->getLocale(),
+                'api'                       =>  'v.1.0',
+                'url'                       =>  url()->current(),
+                'language'                  =>  app()->getLocale(),
+                'is_subscribed'             =>  $is_subscribed,
+                'subscription_end_date'     =>  $subscription_end_date,
             ],
         ];
     }
