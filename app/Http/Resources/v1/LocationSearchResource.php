1641b59d285f97c9341eb23cccbb5e76788277a9
Vishal Prajapati <vishal.p@yudiz.in>
2022-07-11 14:05:06 +0530

    Api: Get Location Changes

---
+++ app/Http/Resources/v1/LocationSearchResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LocationSearchResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            =>  $this->custom_id,
+            'name'          =>  $this->location_name ? $this->location_name : "",
+            'is_active'     =>  $this->is_active,
+        ];
+        return parent::toArray($request);
+    }
+
+    public function with($request)
+    {
+        return [
+            'meta' => [ 
+                'api'               =>  'v.1.0',
+                'url'               =>  url()->current(),
+                'language'          =>  app()->getLocale(),
+            ],
+        ];
+    }
+}
