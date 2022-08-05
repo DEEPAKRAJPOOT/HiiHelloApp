f4fbe3a224020a72be306669de0f519d95bd27c9
Yudiz Solutions Pvt. Ltd <yudiz@Yudizs-Mac-mini-22.local>
2022-08-05 18:27:56 +0530

    user CSV and Subscription CSV file download functionality done

--- app/Http/Controllers/Admin/SubscriptionListController.php
+++ app/Http/Controllers/Admin/SubscriptionListController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Subscription;
 use App\Models\SubscriptionPlan;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 
 class SubscriptionListController extends Controller
 {
@@ -28,9 +30,11 @@ class SubscriptionListController extends Controller
      */
     public function show($custom_id)
     {
-        $subscription = Subscription::with(['user','user.userTransDefault',
-                'subscriptionPlan','subscriptionPlan.subscriptionPlanTransDefault'])
-                ->whereCustomId($custom_id)->firstOrFail();
+        $subscription = Subscription::with([
+            'user', 'user.userTransDefault',
+            'subscriptionPlan', 'subscriptionPlan.subscriptionPlanTransDefault'
+        ])
+            ->whereCustomId($custom_id)->firstOrFail();
         return view('admin.pages.subscription-list.view', ["sub" => $subscription])->with(['custom_title' => 'Subscription']);
     }
 
@@ -38,8 +42,10 @@ class SubscriptionListController extends Controller
     {
         extract($this->DTFilters($request->all()));
         $records = [];
-        $subscriptions = Subscription::with(['subscriptionPlan', 'subscriptionPlan.subscriptionPlanTranslation',
-            'user', 'user.userTransDefault'])->orderBy($sort_column, $sort_order);
+        $subscriptions = Subscription::with([
+            'subscriptionPlan', 'subscriptionPlan.subscriptionPlanTranslation',
+            'user', 'user.userTransDefault'
+        ])->orderBy($sort_column, $sort_order);
 
         if ($search != '') {
             $subscriptions->where(function ($query) use ($search) {
@@ -80,5 +86,78 @@ class SubscriptionListController extends Controller
             ];
         }
         return $records;
+    }
+    public function csvDownload(Request $request)
+    {
+        $subscriptions = Subscription::with('user', 'subscriptionPlan')->get();
+        if (!$subscriptions->isEmpty()) {
+            foreach ($subscriptions as $key => $subscription) {
+                $data[] = [
+                    'Account Id'            =>  $subscription->user->account_id ?? "",
+                    'Name'                  => $subscription->user->userTransEn->full_name ?? "",
+                    'Email'                 =>  $subscription->email ?? "",
+                    'Subscription Plan Name' => $subscription->subscriptionPlan->subscriptionPlanTransEn->name ?? '',
+                    'Months'                 => $subscription->months ?? '',
+                    'Amount'                => $subscription->amount ?? '',
+                    'Start date'            => $subscription->start_date ?? '',
+                    'End date'            => $subscription->end_date ?? '',
+                    'Payment Type'          => $subscription->payment_type ?? '',
+                    'Payment Date'            => $subscription->payment_date ?? '',
+                    'Receipt Data'              => $subscription->receipt_data ?? '',
+                    'Original Transaction Id'   => $subscription->original_transaction_id ?? '',
+                    'Status'                    => $subscription->status ?? '',
+                    'Created at'                => $subscription->created_at ? Carbon::parse($subscription->created_at)->format('o-m-d') : ''
+                ];
+            }
+
+            $filename = "subscriptions-csv.csv";
+            $handle   = fopen($filename, 'w+');
+            fputcsv($handle, array(
+                'Account Id',
+                'Name',
+                'Email',
+                'Subscription Plan Name',
+                'Months',
+                'Amount',
+                'Start date',
+                'End date',
+                'Payment Type',
+                'Payment Date',
+                'Receipt Data',
+                'Original Transaction Id',
+                'Status',
+                'Created at'
+            ));
+
+            foreach ($data as $row) {
+                fputcsv($handle, array(
+                    $row['Account Id'],
+                    $row['Name'],
+                    $row['Email'],
+                    $row['Subscription Plan Name'],
+                    $row['Months'],
+                    $row['Amount'],
+                    $row['Start date'],
+                    $row['End date'],
+                    $row['Payment Type'],
+                    $row['Payment Date'],
+                    $row['Receipt Data'],
+                    $row['Original Transaction Id'],
+                    $row['Status'],
+                    $row['Created at'],
+                ));
+            }
+            fclose($handle);
+
+            $headers = array(
+                'Content-Type' => 'text/csv',
+            );
+
+            // flash('user csv generated successfully!')->success();
+            return Response::download($filename, 'subscriptions-csv.csv', $headers);
+        } else {
+            flash('Unable to generate user csv. Try again later')->error();
+        }
+        return redirect(route('admin.subscription-lists.index'));
     }
 }
