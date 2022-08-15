6ec9cfb4dab7a0b00f8347866018dc15b2f88f20
rinkal_jasani <rinkal.j@yudiz.com>
2022-08-15 16:34:44 +0530

    csv download done

--- app/Http/Controllers/Admin/TrasactionListController.php
+++ app/Http/Controllers/Admin/TrasactionListController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Testing\File;
 
 class TrasactionListController extends Controller
 {
@@ -76,5 +78,64 @@ class TrasactionListController extends Controller
             ];
         }
         return $records;
+    }
+    public function csvDownload(Request $request)
+    {
+        $down_file_name = 'Transactions';
+        $transactions = Transaction::with(['subscriptionPlan', 'user', 'user.userTransDefault', 'subscriptionPlan.subscriptionPlanTranslation'])->get();
+        if (!$transactions->isEmpty()) {
+            foreach ($transactions as $transaction) {
+                $data[] = [
+                    'Account Id'                =>  $transaction->user ? ($transaction->user->account_id ?? "") :  "",
+                    'Name'                      =>  $transaction->user ? ($transaction->user->userTransDefault ? $transaction->user->userTransDefault->full_name : "") : "",
+                    'Email'                     =>  $transaction->email ?? "--",
+                    'Plan Name'                 =>  $transaction->subscriptionPlan ? ($transaction->subscriptionPlan->subscriptionPlanTranslation ? $transaction->subscriptionPlan->subscriptionPlanTranslation->name : "N/A") : "",
+                    'Months'                    =>  $transaction->subscriptionPlan ? ($transaction->subscriptionPlan ? $transaction->subscriptionPlan->months : "N/A") : "",
+                    'Amount'                    =>  $transaction->subscriptionPlan ? ($transaction->subscriptionPlan ? $transaction->subscriptionPlan->amount : "N/A") : "",                    
+                    'Payment Type'              =>  $transaction->payment_type ?? "",
+                    'Razorpay Order Id'         =>  $transaction->razorpay_order_id ?? "",
+                    'Razorpay Paymentb Id'              =>  $transaction->razorpay_payment_id ?? "",
+                    'Razorpay Signature'              =>  $transaction->razorpay_signature ?? "",
+                    'Transaction Id'              =>  $transaction->transaction_id ?? "",
+                    'Original Transaction Id'              =>  $transaction->original_transaction_id ?? "",
+                    'Web Order Line Item Id'              =>  $transaction->web_order_line_item_id ?? "",
+                    'Purchase Date'              =>  $transaction->purchase_date ?? "",
+                    'Original Purchase Date'              =>  $transaction->original_purchase_date ?? "",
+                    'Subscription End DAte'              =>  $transaction->subscription_end_date ?? "",
+                    'Receipt Data'              =>  $transaction->receipt_data ?? "",
+                    'In App Ownership Type'              =>  $transaction->in_app_ownership_type ?? "",
+                    'Subscription Group Identifier'              =>  $transaction->subscription_group_identifier ?? "",
+                    'Status'                    =>  $transaction->status ?? "",
+                    'Created at'                =>  $transaction->created_at ? Carbon::parse($transaction->created_at)->format('Y-m-d') : ""
+                ];
+                dd($data);
+            }
+
+            if (!File::exists(public_path() . "/files")) {
+                File::makeDirectory(public_path() . "/files");
+            }
+
+            $filename = public_path('files/' . $down_file_name . ".csv");
+            $handle   = fopen($filename, 'w+');
+            fputcsv($handle, array(
+                'Account Id', 'Name', 'Email', 'Subscription Plan Name', 'Months', 'Amount', 'Start date', 'End date', 'Payment Type', 'Payment Date', 'Receipt Data', 'Original Transaction Id', 'Status', 'Created at'
+            ));
+
+            foreach ($data as $row) {
+                fputcsv($handle, array(
+                    $row['Account Id'], $row['Name'], $row['Email'], $row['Subscription Plan Name'], $row['Months'], $row['Amount'], $row['Start date'], $row['End date'], $row['Payment Type'], $row['Payment Date'], $row['Receipt Data'], $row['Original Transaction Id'], $row['Status'], $row['Created at'],
+                ));
+            }
+            fclose($handle);
+
+            $headers = array(
+                'Content-Type' => 'text/csv',
+            );
+
+            return Response::download($filename, $down_file_name . ".csv", $headers);
+        } else {
+            flash('Unable to generate transaction csv file. Try again later')->error();
+        }
+        return redirect(route('admin.subscription-lists.index'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::post('profile-details/csv-upload','Admin\ProfileDetailController@csvUploa
 Route::get('profile-details/sample-csv-download','Admin\ProfileDetailController@sampleCsvDownload')->name('admin.profile-details.sample-csv-download');
 Route::get('users/csv-download', 'Admin\UsersController@csvDownload')->name('admin.users.csv-download');
 Route::get('subscription-lists/csv-download', 'Admin\SubscriptionListController@csvDownload')->name('admin.subscriptions.csv-download');
+Route::get('transaction/csv-download', 'Admin\TransactionListController@csvDownload')->name('admin.transactions.csv-download');
 
 /* CMS Pages */
   Route::get('about-us/{device?}', 'FrontendPagesController@about')->name('about.us');
