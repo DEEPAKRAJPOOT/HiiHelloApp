677cf6699f31d7eb4f4debb00cb98b25eff07d2a
Vishal Prajapati <vishal.p@yudiz.in>
2022-08-26 15:12:56 +0530

    CDN: Image CDN Using S3 Bucket

--- app/Http/Controllers/Admin/HelperController.php
+++ app/Http/Controllers/Admin/HelperController.php
@@ -18,6 +18,13 @@ class HelperController extends Controller
             $url = Storage::url($path);
 
         // $url = Storage::temporaryUrl( $path, now()->addMinutes(5) );
+
+        // For AWS CDN
+        if( !empty(env('AWS_URl')) && !empty(env('FILESYSTEM_DRIVER')) && env('FILESYSTEM_DRIVER') == 's3' ){
+            // $url = Storage::disk('s3')->url($path);
+            $url = env('AWS_URl').$path;
+        }
+
         return $url;
     }
 }
