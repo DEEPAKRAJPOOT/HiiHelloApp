2ed615ffd1346a7723e815b53f870d8a91d5749f
Ubuntu <ubuntu@ip-172-31-13-154.ap-south-1.compute.internal>
2022-10-14 10:28:39 +0000

    local changes

--- routes/admin.php
+++ routes/admin.php
@@ -17,6 +17,18 @@ Route::group(['middleware' => ['revalidate']], function () {
 	
 });
 
+// cache clear
+Route::get('/cache', function () {
+	Artisan::call('config:clear');
+	Artisan::call('route:clear');
+	Artisan::call('view:clear');
+	Artisan::call('event:clear');
+	Artisan::call('cache:clear');
+	Artisan::call('config:clear');
+	Artisan::call('optimize');
+	dd("Cache is cleared");
+});
+
 
 Route::group(['namespace' => 'Admin', 'middleware' => ['check_permit', 'revalidate']], function () {
 	
