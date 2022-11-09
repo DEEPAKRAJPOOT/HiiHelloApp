948647464333e87362b5bd9c3f1c6e345db82ea7
Anand Kamani <anandpatel8511@gmail.com>
2022-11-09 11:07:05 +0530

    Dashboard No. of subscriptions speed table optimize

--- app/Http/Controllers/Admin/PagesController.php
+++ app/Http/Controllers/Admin/PagesController.php
@@ -148,7 +148,6 @@ class PagesController extends Controller
         $user['city_result'] = [];
         $user['subscription_result'] = $subscription_result;
         $user['age_result'] = $age_result;
-        // echo "<pre>"; print_r($location_result); die();
         return view('admin.pages.general.dashboard', compact('user'))->with(['custom_title' => __('Dashboard')]);
     }
     public function profile()
