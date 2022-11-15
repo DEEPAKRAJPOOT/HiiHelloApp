c68db53109037d6ded983fd1104842c6103fdfc9
Anand Kamani <anandpatel8511@gmail.com>
2022-11-15 15:28:52 +0530

    Debug translate location add code

--- app/Http/Controllers/UtilityController.php
+++ app/Http/Controllers/UtilityController.php
@@ -204,19 +204,21 @@ class UtilityController extends Controller
     {   
         $limit = $request->limit ? $request->limit : 1;
         $is_print = $request->is_print ? $request->is_print : 0;
-        
+        $id = $request->id ? $request->id : '';
+
         $default_lang_code  =   config('utility.default_lang_code');
         $apiKey             =   config('utility.google.translate.api_key');
         $message            =   'No details found to translate !!!';
         $message            =   "No location translate records found.";
 
-        $locations  = DB::table("location_translations")
-                    ->where("locale","!=","en")
-                    // ->where("id","=",47067)
-                    // ->skip(20)
-                    ->limit($limit)
-                    ->orderBy("name","ASC")
-                    ->get();
+        $locations  = DB::table("location_translations");
+        $locations  = $locations->where("locale","!=","en");
+        if (!empty($id)) {
+            $locations  = $locations->where("id","=",$id);
+        }
+        $locations  = $locations->limit($limit);
+        $locations  = $locations->orderBy("name","ASC");
+        $locations  = $locations->get();
         if ($is_print == 1) {
             echo "<pre>"; print_r($locations->toArray()); die();
         }
