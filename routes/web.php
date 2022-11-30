f1a97d82651af30cd5dc7d4df59eca248a79c648
Anand Kamani <anandpatel8511@gmail.com>
2022-11-30 15:31:57 +0530

    Location wise no of users report downlode csv

--- routes/web.php
+++ routes/web.php
@@ -53,6 +53,7 @@ Route::get('transaction/csv-download', 'Admin\TrasactionListController@csvDownlo
 Route::get('profile-report/csv-download', 'Admin\ProfileReportController@csvDownload')->name('admin.profile-report.csv-download');
 Route::get('call-log/csv-download', 'Admin\CallController@csvDownload')->name('admin.call-log.csv-download');
 
+Route::get('location/csv-download', 'Admin\LocationController@csvDownload')->name('admin.location.csv-download');
 /* CMS Pages */
   Route::get('about-us/{device?}', 'FrontendPagesController@about')->name('about.us');
   Route::get('terms-and-conditions/{device?}', 'FrontendPagesController@terms')->name('terms');
