284f6fe9b94bf0f6aba29bd85ebc2a8bc3580eba
Kanak-ST <parmar.mansukhbhai@saturdaytechnologies.io>
2022-12-21 18:00:45 +0530

    Filter box for today/weekly added also added date range filter and account id in listing

--- app/Http/Controllers/Admin/ImageModerationController.php
+++ app/Http/Controllers/Admin/ImageModerationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use DB;
 use App\Http\Controllers\Controller;
 use App\Models\ImageModerationLog;
 use Carbon\Carbon;
@@ -26,16 +27,29 @@ class ImageModerationController extends Controller
     public function listing(Request $request)
     {
         extract($this->DTFilters($request->all()));
-        $records = [];
-        //$call_logs = ImageModerationLog::with(['userDetails'])->orderBy($sort_column, $sort_order);
 
-        $call_logs = ImageModerationLog::select('image_moderation_log.*','user_translations.full_name as full_name');
-        $call_logs->leftJoin('user_translations', 'user_translations.user_id', '=', 'image_moderation_log.user_id')->where('locale','en')->orderBy($sort_column, $sort_order);
+        $from_date         = ($request->from_date) ? $request->from_date." 00:00:00" : "";
+        $to_date           = ($request->to_date) ? $request->to_date." 23:59:59" : "";
+
+
+        $records = [];
+        
+        
+        $query_field_approve = DB::raw("(CASE WHEN is_approved='1' THEN 'Approve' ELSE 'Decline' END) as approve_staus");
+
+
+        $image_logs = ImageModerationLog::select('image_moderation_log.*','user_translations.full_name as full_name',$query_field_approve);
+        $image_logs->leftJoin('user_translations', 'user_translations.user_id', '=', 'image_moderation_log.user_id')->where('locale','en')->orderBy($sort_column, $sort_order);
+
+        // ST - Filter
+        if($from_date != "" && $to_date != "") {
+            $image_logs = $image_logs->whereBetween('created_at', [$from_date, $to_date]);
+        }
 
         
 
         if ($search != '') {
-            $call_logs->where(function ($query) use ($search, $call_logs) {
+            $image_logs->where(function ($query) use ($search, $image_logs) {
                 $query->where('created_at', 'like', "%{$search}%")->orwhere('endpoint_url', 'like', "%{$search}%")->orwhere('message', 'like', "%{$search}%")
                     ->orWhereHas('userDetails.userTranslations', function ($query1) use ($search) {
                         $query1->where('full_name', 'like', "%{$search}%");
@@ -44,56 +58,104 @@ class ImageModerationController extends Controller
             });
         }
 
-        $count = $call_logs->count();
+        $count = $image_logs->count();
         $records['recordsTotal'] = $count;
         $records['recordsFiltered'] = $count;
         $records['data'] = [];
 
         // $call_logs = $call_logs->offset($offset)->limit($limit)->orderBy($sort_column, $sort_order);
-        $call_logs = $call_logs->get();
+        $image_logs = $image_logs->get();
+ 
 
-       
-
-        foreach ($call_logs as $call_log) {
-
+        foreach ($image_logs as $image_log) {
             
                 $records['data'][] = [
-                'full_name'             => $call_log->userDetails->full_name,
-                'message'               => $call_log->message,
-                'created_at'            => date('Y-m-d H:i:s',strtotime($call_log->created_at)),
-                'is_approved'           => $call_log->is_approved > 0 ? "Approved" : "Un Approved",
-                'total_face_detected'   => $call_log->total_face_detected,                
-                'endpoint_url'          => $call_log->endpoint_url, 
+                'account_id'            => $image_log->userDetails->account_id,
+                'full_name'             => $image_log->userDetails->full_name,
+                'message'               => $image_log->message,
+                'created_at'            => date('Y-m-d H:i:s',strtotime($image_log->created_at)),
+                'is_approved'           => $image_log->approve_staus,
+                'total_face_detected'   => $image_log->total_face_detected,                
+                'endpoint_url'          => $image_log->endpoint_url, 
 
             ];
         }
+
+        //GET TODAYS TOTAL APPROVED RECORD
+        $today_start_date = date("Y-m-d")." 00:00:00";        
+        $today_end_date = date("Y-m-d")." 23:59:59";
+        $count_today_approved = $this->getImageLogSummury($today_start_date,$today_end_date,1);
+        $count_today_decline = $this->getImageLogSummury($today_start_date,$today_end_date,0);
+
+
+        //GET WEEKLY TOTAL APPROVED RECORD
+        $today_start_date =  date('Y-m-d', strtotime('-7 days'))." 00:00:00";        
+        $today_end_date = date("Y-m-d")." 23:59:59";
+
+        $count_weekly_approved = $this->getImageLogSummury($today_start_date,$today_end_date,1);
+        $count_weekly_decline = $this->getImageLogSummury($today_start_date,$today_end_date,0);
+
+
+        $records['summury']['today_approved'] = $count_today_approved;
+        $records['summury']['today_decline'] = $count_today_decline;
+        $records['summury']['weekly_approved'] = $count_weekly_approved;
+        $records['summury']['weekly_decline'] = $count_weekly_decline;
+
+        
         return $records;
+        
     }
 
     public function csvDownload(Request $request)
     {
+
+        $from_date         = ($request->from_date_hidden) ? $request->from_date_hidden." 00:00:00" : "";
+        $to_date           = ($request->to_date_hidden) ? $request->to_date_hidden." 23:59:59" : "";
+        $search            = ($request->search_data_hidden) ? $request->search_data_hidden : "";
+
+        
+
         
         $down_file_name = 'Image Moderation Log';
-        //$call_logs = CallLog::with('room','room.creator.userTransEn', 'room.participator.userTransEn', 'room.creator','room.participator')->get();
-
-
         $records = [];
-        //$call_logs = ImageModerationLog::with(['userDetails'])->orderBy($sort_column, $sort_order);
-
-        $call_logs = ImageModerationLog::select('image_moderation_log.*','user_translations.full_name as full_name','user_translations.full_name as full_name');
-        $call_logs->leftJoin('user_translations', 'user_translations.user_id', '=', 'image_moderation_log.user_id')->where('locale','en');
 
         
-        $call_logs = $call_logs->get();
+        $query_field_approve = DB::raw("(CASE WHEN is_approved='1' THEN 'Approve' ELSE 'Decline' END) as approve_staus");
 
-        if (!$call_logs->isEmpty()) {
-            foreach ($call_logs as $call_log) {
+
+        $image_logs = ImageModerationLog::select('image_moderation_log.*','user_translations.full_name as full_name',$query_field_approve);
+        $image_logs->leftJoin('user_translations', 'user_translations.user_id', '=', 'image_moderation_log.user_id')->where('locale','en');
+
+        // ST - Filter
+        if($from_date != "" && $to_date != "") {
+            $image_logs = $image_logs->whereBetween('created_at', [$from_date, $to_date]);
+        }
+
+        
+
+        if ($search != '') {
+            $image_logs->where(function ($query) use ($search, $image_logs) {
+                $query->where('created_at', 'like', "%{$search}%")->orwhere('endpoint_url', 'like', "%{$search}%")->orwhere('message', 'like', "%{$search}%")
+                    ->orWhereHas('userDetails.userTranslations', function ($query1) use ($search) {
+                        $query1->where('full_name', 'like', "%{$search}%");
+                    });
+                    
+            });
+        }
+
+        
+        $image_logs = $image_logs->get();
+
+        
+
+        if (!$image_logs->isEmpty()) {
+            foreach ($image_logs as $call_log) {
                 $data[] = [
-                    'user_id'               => $call_log->user_id,
+                    'user_id'               => $call_log->userDetails->account_id,
                     'full_name'             => $call_log->full_name,
                     'message'               => $call_log->message,
                     'created_at'            => date('Y-m-d H:i:s',strtotime($call_log->created_at)),
-                    'is_approved'           => $call_log->is_approved > 0 ? "Approved" : "Un Approved",
+                    'is_approved'           => $call_log->approve_staus,
                     'total_face_detected'   => $call_log->total_face_detected,   
                     'endpoint_url'          => $call_log->endpoint_url,                                  
                     'image_type'            => Ucfirst(str_replace("_"," ",$call_log->image_type)),      
@@ -123,6 +185,20 @@ class ImageModerationController extends Controller
         } else {
             flash('Unable to generate Call logs csv file. Try again later')->error();
         }
-        return redirect(route('admin.image-logs.index'));
+        return redirect(route('admin.image-logs'));
     }
+
+    public static function getImageLogSummury($summury_start_date,$summury_end_date,$is_approved)
+    {
+
+        $image_today_approve = ImageModerationLog::select('id')->where('is_approved',$is_approved);
+        if($summury_start_date != "" && $summury_end_date != "") {
+            $image_today_approve = $image_today_approve->whereBetween('created_at', [$summury_start_date, $summury_end_date]);
+        }
+        $summury_count = $image_today_approve->count();
+
+        return $summury_count;
+
+    }
+
 }
--- resources/views/admin/pages/image-moderation-logs/index.blade.php
+++ resources/views/admin/pages/image-moderation-logs/index.blade.php
@@ -10,6 +10,83 @@
 
 @section('content')
 <div class="container">
+    <div class="row">
+        <div class="col-lg-3">
+           <div class="card card-custom rounded-xl gutter-b bg-dark card-stretch">
+              <div class="d-flex align-items-center mr-2">
+                 <div class="symbol-label px-6 py-8 rounded-xl mr-7">
+                   <span class="svg-icon svg-icon-4x svg-icon-white d-block my-2"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo4/dist/../src/media/svg/icons/Design/Image.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <polygon points="0 0 24 0 24 24 0 24"/>
+        <path d="M6,5 L18,5 C19.6568542,5 21,6.34314575 21,8 L21,17 C21,18.6568542 19.6568542,20 18,20 L6,20 C4.34314575,20 3,18.6568542 3,17 L3,8 C3,6.34314575 4.34314575,5 6,5 Z M5,17 L14,17 L9.5,11 L5,17 Z M16,14 C17.6568542,14 19,12.6568542 19,11 C19,9.34314575 17.6568542,8 16,8 C14.3431458,8 13,9.34314575 13,11 C13,12.6568542 14.3431458,14 16,14 Z" fill="#000000"/>
+    </g>
+</svg><!--end::Svg Icon--></span>
+                    <span class="font-size-h6 text-muted font-weight-bold">Today's Approved</span>
+                 </div>
+                 <div>
+                    <div class="font-size-h1 text-white font-weight-bolder" id="today_approve_total">Processing...</div>
+                 </div>
+              </div>
+           </div>
+        </div>
+
+         <div class="col-lg-3">
+           <div class="card card-custom rounded-xl gutter-b bg-dark card-stretch">
+              <div class="d-flex align-items-center mr-2">
+                 <div class="symbol-label px-6 py-8 rounded-xl mr-7">
+                   <span class="svg-icon svg-icon-4x svg-icon-white d-block my-2"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo4/dist/../src/media/svg/icons/Design/Image.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <polygon points="0 0 24 0 24 24 0 24"/>
+        <path d="M6,5 L18,5 C19.6568542,5 21,6.34314575 21,8 L21,17 C21,18.6568542 19.6568542,20 18,20 L6,20 C4.34314575,20 3,18.6568542 3,17 L3,8 C3,6.34314575 4.34314575,5 6,5 Z M5,17 L14,17 L9.5,11 L5,17 Z M16,14 C17.6568542,14 19,12.6568542 19,11 C19,9.34314575 17.6568542,8 16,8 C14.3431458,8 13,9.34314575 13,11 C13,12.6568542 14.3431458,14 16,14 Z" fill="#000000"/>
+    </g>
+</svg><!--end::Svg Icon--></span>
+                    <span class="font-size-h6 text-muted font-weight-bold">Today's Decline</span>
+                 </div>
+                 <div>
+                    <div class="font-size-h1 text-white font-weight-bolder" id="today_decline_total">Processing...</div>
+                 </div>
+              </div>
+           </div>
+        </div>
+
+         <div class="col-lg-3">
+           <div class="card card-custom rounded-xl gutter-b bg-dark card-stretch">
+              <div class="d-flex align-items-center mr-2">
+                 <div class="symbol-label px-6 py-8 rounded-xl mr-7">
+                   <span class="svg-icon svg-icon-4x svg-icon-white d-block my-2"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo4/dist/../src/media/svg/icons/Design/Image.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <polygon points="0 0 24 0 24 24 0 24"/>
+        <path d="M6,5 L18,5 C19.6568542,5 21,6.34314575 21,8 L21,17 C21,18.6568542 19.6568542,20 18,20 L6,20 C4.34314575,20 3,18.6568542 3,17 L3,8 C3,6.34314575 4.34314575,5 6,5 Z M5,17 L14,17 L9.5,11 L5,17 Z M16,14 C17.6568542,14 19,12.6568542 19,11 C19,9.34314575 17.6568542,8 16,8 C14.3431458,8 13,9.34314575 13,11 C13,12.6568542 14.3431458,14 16,14 Z" fill="#000000"/>
+    </g>
+</svg><!--end::Svg Icon--></span>
+                    <span class="font-size-h6 text-muted font-weight-bold">Weekly Approved</span>
+                 </div>
+                 <div>
+                    <div class="font-size-h1 text-white font-weight-bolder" id="weekly_approve_total">Processing...</div>
+                 </div>
+              </div>
+           </div>
+        </div>
+
+         <div class="col-lg-3">
+           <div class="card card-custom rounded-xl gutter-b bg-dark card-stretch">
+              <div class="d-flex align-items-center mr-2">
+                 <div class="symbol-label px-6 py-8 rounded-xl mr-7">
+                   <span class="svg-icon svg-icon-4x svg-icon-white d-block my-2"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo4/dist/../src/media/svg/icons/Design/Image.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <polygon points="0 0 24 0 24 24 0 24"/>
+        <path d="M6,5 L18,5 C19.6568542,5 21,6.34314575 21,8 L21,17 C21,18.6568542 19.6568542,20 18,20 L6,20 C4.34314575,20 3,18.6568542 3,17 L3,8 C3,6.34314575 4.34314575,5 6,5 Z M5,17 L14,17 L9.5,11 L5,17 Z M16,14 C17.6568542,14 19,12.6568542 19,11 C19,9.34314575 17.6568542,8 16,8 C14.3431458,8 13,9.34314575 13,11 C13,12.6568542 14.3431458,14 16,14 Z" fill="#000000"/>
+    </g>
+</svg><!--end::Svg Icon--></span>
+                    <span class="font-size-h6 text-muted font-weight-bold">Weekly Decline</span>
+                 </div>
+                 <div>
+                    <div class="font-size-h1 text-white font-weight-bolder" id="weekly_decline_total">Processing...</div>
+                 </div>
+              </div>
+           </div>
+        </div>
+    </div>
 
     <div class="card card-custom">
         <div class="card-header">
@@ -34,18 +111,53 @@
                     Add {{ $custom_title }}
                 </a>
                 @endif --}}
-                <a href="{{ route('admin.image-log.csv-download') }}"
-                    class="btn btn-sm btn-primary font-weight-bolder text-uppercase ml-2">
+
+                <a href="javascript:void(0)"
+                    class="btn btn-sm btn-primary font-weight-bolder text-uppercase ml-2 clsDownloadCsv">
                     <i class="fas fa-arrow-down"></i>
                     Download CSV
                 </a>
             </div>
         </div>
         <div class="card-body">
+            <form id="frmcsv"  name="frmcsv" action="{{ route('admin.image-log.csv-download') }}" method="get" enctype="multipart">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                <input type="hidden" name="from_date_hidden" id="from_date_hidden" value="">
+                <input type="hidden" name="to_date_hidden" id="to_date_hidden" value="">
+                <input type="hidden" name="search_data_hidden" id="search_data_hidden" value="">
+            </form>
+            {{-- Filter Start --}}
+            <table class="mb-5" align="center">                
+                <tr>
+                    <td>
+                        <span class="card-icon">
+                            <i class="fa fa-filter text-primary"></i>
+                        </span>
+                        <label>Filter:&nbsp;&nbsp;</label>
+                    </td>
+                    <td>                        
+                        <input type='date' id='search_fromdate' class="form-control" placeholder='From date' value="{{ date('Y-m-d') }}">
+                    </td>
+                    <td>
+                        <input type='date' id='search_todate' class="form-control" placeholder='To date' value="{{ date('Y-m-d') }}">
+                    </td>
+                    
+                    <td>
+                        <input type='button' class="btn btn-primary mr-1 ml-1" id="btn_search_filter" value="Search">
+                    </td>
+                    <td>
+                        <a href="javascript:;" class="btn btn-warning" id="btn_reset_filter">Reset</a>
+                    </td>
+                </tr>
+            </table>
+            {{-- Filter End --}}
+
+
             {{-- Datatable Start --}}
             <table class="table table-bordered table-hover table-checkable" id="image_logs_table"
                 style="margin-top: 13px !important"></table>
             {{-- Datatable End --}}
+           
         </div>
     </div>
 </div>
@@ -61,13 +173,47 @@
             searchDelay: 500,
             processing: true,
             serverSide: true,
+            cache: false,
             ajax: {
                 url: "{{ route('admin.image-logs.listing') }}",
                 data: {
-                    columnsDef: ['full_name', 'message', 'created_at','is_approved','total_face_detected','endpoint_url'],
-                },
+                    columnsDef: ['account_id','full_name', 'message', 'created_at','is_approved','total_face_detected','endpoint_url'],
+                },                
+                data: function(data) {                    
+
+                    // ST - Filter Params
+                    var from_date       = $("#search_fromdate").val();
+                    var to_date         = $("#search_todate").val();  
+                        
+                    // EN - Filter Params
+
+                    data.from_date         = from_date;
+                    data.to_date           = to_date;                    
+                    //data.flgPendingProfile = $(".getpendingprofile").is(':checked') ? 1 : 0;
+               },               
+               complete: function(response) {
+                    //console.log(response);
+                    if (response != '') {
+
+                        var summury_data  = JSON.parse(response.responseText).summury;
+
+                        //console.log(summury_data);
+                        /*
+                        console.log("Result " + summury_data.today_approved);
+                        console.log("Result " + summury_data.today_decline);
+                        console.log("Result " + summury_data.weekly_approved);
+                        console.log("Result " + summury_data.weekly_decline);
+                        */
+
+                        $("#today_approve_total").html(summury_data.today_approved);
+                        $("#today_decline_total").html(summury_data.today_decline);
+                        $("#weekly_approve_total").html(summury_data.weekly_approved);
+                        $("#weekly_decline_total").html(summury_data.weekly_decline); 
+                    }  
+              },
             },
             columns: [
+                { data: 'account_id' },
                 { data: 'full_name' },
                 { data: 'message' },
                 { data: 'created_at' },
@@ -77,17 +223,18 @@
             ],
             columnDefs: [
                 // Specify columns titles here...
-                { targets: 0, title: "User Name", orderable: true },
-                { targets: 1, title: "Message", orderable: false },
-                { targets: 2, title: "Date", orderable: true },
-                { targets: 3, title: 'Status', orderable: true },
-                { targets: 4, title: 'Total Face Detected', orderable: false },
+                { targets: 0, title: "Account Id", orderable: true },
+                { targets: 1, title: "User Name", orderable: true },
+                { targets: 2, title: "Message", orderable: false },
+                { targets: 3, title: "Date", orderable: true },
+                { targets: 4, title: 'Status', orderable: true },
+                { targets: 5, title: 'Total Face Detected', orderable: false },
                 // Action buttons
                 { targets: -1, title: 'End Point',
                 orderable: false },
             ],
             order: [
-                [2, 'desc']
+                [3, 'desc']
             ],
             lengthMenu: [
                 [10, 20, 50, 100],
@@ -96,5 +243,36 @@
             pageLength: 10,
         });
     });
+
+    $(document).on("click", "#btn_search_filter", function () {
+        
+        oTable.draw();
+    });
+
+    $(document).on("click", "#btn_reset_filter", function () {
+        $("#search_fromdate,#search_todate,#gender_filter").val('');
+        oTable.draw();
+    });
+
+    $(document).on("click", ".clsDownloadCsv", function () {             
+         var from_date       = $("#search_fromdate").val();
+         var to_date         = $("#search_todate").val();  
+         var input           = $('.dataTables_filter input')[0];  
+
+         var search_data     = input.value;
+         console.log("Data" + search_data);
+         
+         $('#from_date_hidden').val(from_date);
+         $('#to_date_hidden').val(to_date);
+         $('#search_data_hidden').val(search_data);
+         
+         $("#frmcsv").submit();
+
+         //window.location = "{{ route('admin.image-log.csv-download') }}";
+         
+
+    });
+
+
 </script>
 @endpush
