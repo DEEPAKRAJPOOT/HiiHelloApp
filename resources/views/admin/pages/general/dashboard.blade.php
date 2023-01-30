1f05859c3013367c27d8c0ae301a1dfb4d86a152
Rizwan <rizwan@local.com>
2023-01-30 14:45:24 +0530

    CHANGE: re-enable dashboard all cards and graphs

--- resources/views/admin/pages/general/dashboard.blade.php
+++ resources/views/admin/pages/general/dashboard.blade.php
@@ -162,8 +162,8 @@
               <option value="3 month">3 Months</option>
               <option value="6 month">6 Months</option>
               <option value="year">This Year</option>
-              {{-- <option value="overall">Overall</option>
-              <option value="custom">Custom Range</option> --}}
+              <option value="overall">Overall</option>
+              <option value="custom">Custom Range</option>
             </select>
           </div>
         </div>
@@ -296,7 +296,7 @@
 
   </div>
 
-  {{-- <div class="row">
+  <div class="row">
     <div class="col-lg-12">
       <div class="card card-custom gutter-b">
         <div class="card-header flex-wrap border-0 py-5">
@@ -650,9 +650,9 @@
         </div>
       </div>
     </div>
-  </div> --}}
+  </div>
 
-{{-- @if(auth()->user()->type == 'admin')
+@if(auth()->user()->type == 'admin')
   <!--end::second raw-->
   <!--begin::third row-->
   <div class="row">
@@ -676,7 +676,7 @@
     </div>
   </div>
   <!--end::third raw-->
-@endif --}}
+@endif
 </div>
 @endsection
 @push('extra-js-scripts')
@@ -923,7 +923,7 @@ $(document).ready(function() {
 </script>
 
 <!-- language dt -->
-{{-- <script type="text/javascript">
+<script type="text/javascript">
   $(function() {
     var table = $('#language_DT');
 
@@ -982,7 +982,7 @@ $(document).ready(function() {
 
 
   });
-</script> --}}
+</script>
 
 <!-- mode of registration with no of users listing dt -->
 <script type="text/javascript">
