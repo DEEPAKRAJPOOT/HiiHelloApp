39d27d845083ccc9336c935cb3a6b0ec7566a4e1
Rizwan <rizwan@local.com>
2023-01-31 11:34:31 +0530

    FIXED: cron issue of full name

--- app/Console/Commands/GoogleTranslation.php
+++ app/Console/Commands/GoogleTranslation.php
@@ -2,9 +2,11 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
+use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class GoogleTranslation extends Command
 {
@@ -43,53 +45,77 @@ class GoogleTranslation extends Command
         $default_lang_code  =   config('utility.default_lang_code');
         $apiKey             =   config('utility.google.translate.api_key');
         $message            =   'No details found to translate !!!';
-
         // $users = User::select('id','custom_id','language_id','is_trans_full_name','is_trans_about_me','is_trans_fav_movie')
         //                         ->with('userTranslations','language')
         //                         ->where('is_trans_full_name','n')->orWhere('is_trans_about_me','n')
         //                         ->orWhere('is_trans_fav_movie','n')->get();
 
-        User::select('id','custom_id','language_id','is_trans_full_name','is_trans_about_me','is_trans_fav_movie')
-                                ->with('userTranslations','language')
-                                ->where('is_trans_full_name','n')->orWhere('is_trans_about_me','n')
-                                ->orWhere('is_trans_fav_movie','n')
-                                ->chunk(100, function($users) use ($language_alloweds, $apiKey, $message) {
-            if($users->isNotEmpty()){
-                foreach($users as $user){
-                    if($user->userTranslations->isNotEmpty()){
-                        $detected_lang = $user->language ? $user->language->lang_code : 'en';
-                        // $detected_lang = $user->userTranslations[0] ? $user->userTranslations[0]->locale : $detected_lang;
+        $users = User::select('id', 'custom_id', 'is_trans_full_name')
+            ->with('userTranslationOnlyOne')
+            ->where('is_trans_full_name', 'n')
+            ->orderBy('created_at', "asc")
+            ->limit(10)
+            ->get();
 
-                        if($user->userTranslations[0]){
-                            $full_name  =   $user->userTranslations[0]->full_name;
-                            $about_me   =   $user->userTranslations[0]->about_me;
-                            $fav_movie  =   $user->userTranslations[0]->fav_movie;
+        foreach ($users as $user) {
+            if ($user->userTranslationOnlyOne) {
+                $detected_lang = null;
 
-                            if( !empty($full_name) && $user->is_trans_full_name == 'n'){
-                                $message = $this->translateText($apiKey, $language_alloweds, $user, $detected_lang, 'full_name', $full_name );
-                                $user->is_trans_full_name = 'y';
-                            }
-                            // if( !empty($about_me) && $user->is_trans_about_me == 'n'){
-                            //     $message = $this->translateText($apiKey, $language_alloweds, $user, $detected_lang, 'about_me', $about_me );
-                            //     $user->is_trans_about_me = 'y';
-                            // }
-                            // if( !empty($fav_movie) && $user->is_trans_fav_movie == 'n'){
-                            //     $message = $this->translateText($apiKey, $language_alloweds, $user, $detected_lang, 'fav_movie', $fav_movie );
-                            //     $user->is_trans_fav_movie = 'y';
-                            // }
+                if ($user->userTranslationOnlyOne->full_name) {
+                    $full_name  =   $user->userTranslationOnlyOne->full_name;
+                    // $about_me   =   $user->userTranslations[0]->about_me;
+                    // $fav_movie  =   $user->userTranslations[0]->fav_movie;
 
-                            $user->save();
-                        }
+                    if (!empty($full_name) && $user->is_trans_full_name == 'n') {
+                        $message = $this->translateText($apiKey, $language_alloweds, $user, $detected_lang, 'full_name', $full_name);
+                        $user->is_trans_full_name = 'y';
                     }
+                    $user->save();
                 }
             }
-        });
+        }
+
+        // User::select('id', 'custom_id', 'language_id', 'is_trans_full_name', 'is_trans_about_me', 'is_trans_fav_movie')
+        //     ->with('userTranslations', 'language')
+        //     ->where('is_trans_full_name', 'n')->orWhere('is_trans_about_me', 'n')
+        //     ->orWhere('is_trans_fav_movie', 'n')
+        //     ->chunk(100, function ($users) use ($language_alloweds, $apiKey, $message) {
+        //         if ($users->isNotEmpty()) {
+        //             foreach ($users as $user) {
+        //                 if ($user->userTranslations->isNotEmpty()) {
+        //                     $detected_lang = $user->language ? $user->language->lang_code : 'en';
+        //                     // $detected_lang = $user->userTranslations[0] ? $user->userTranslations[0]->locale : $detected_lang;
+
+        //                     if ($user->userTranslations[0]) {
+        //                         $full_name  =   $user->userTranslations[0]->full_name;
+        //                         $about_me   =   $user->userTranslations[0]->about_me;
+        //                         $fav_movie  =   $user->userTranslations[0]->fav_movie;
+
+        //                         if (!empty($full_name) && $user->is_trans_full_name == 'n') {
+        //                             $message = $this->translateText($apiKey, $language_alloweds, $user, $detected_lang, 'full_name', $full_name);
+        //                             $user->is_trans_full_name = 'y';
+        //                         }
+        //                         // if( !empty($about_me) && $user->is_trans_about_me == 'n'){
+        //                         //     $message = $this->translateText($apiKey, $language_alloweds, $user, $detected_lang, 'about_me', $about_me );
+        //                         //     $user->is_trans_about_me = 'y';
+        //                         // }
+        //                         // if( !empty($fav_movie) && $user->is_trans_fav_movie == 'n'){
+        //                         //     $message = $this->translateText($apiKey, $language_alloweds, $user, $detected_lang, 'fav_movie', $fav_movie );
+        //                         //     $user->is_trans_fav_movie = 'y';
+        //                         // }
+
+        //                         $user->save();
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     });
 
         // $this->info($message);
         return $message;
     }
 
-    function translateText($apiKey, $language_alloweds, $user, $detected_lang, $column, $text )
+    function translateText($apiKey, $language_alloweds, $user, $detected_lang, $column, $text)
     {
         $message = 'No details found to translate !!!';
 
@@ -104,12 +130,12 @@ class GoogleTranslation extends Command
 
         // if($responseCode == 200) {
         //     if($responseDecoded['data'] && $responseDecoded['data']['detections'] && $responseDecoded['data']['detections'][0] && $responseDecoded['data']['detections'][0][0] && $responseDecoded['data']['detections'][0][0]['language']){
-                
+
         //         $detected_lang = $responseDecoded['data']['detections'][0][0]['language'] ?? $detected_lang;
         //     }
         // }
 
-        foreach($language_alloweds as $language_allowed){
+        foreach ($language_alloweds as $language_allowed) {
 
             // Translate Language
             // if ($detected_lang == $language_allowed) {
@@ -120,8 +146,8 @@ class GoogleTranslation extends Command
             //     $traslate_url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source='.$detected_lang.'&target='.$language_allowed;
             // }
 
-            $traslate_url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&target='.$language_allowed;
-            
+            $traslate_url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&target=' . $language_allowed;
+
             $handle = curl_init($traslate_url);
             curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
             $response = curl_exec($handle);
@@ -129,7 +155,7 @@ class GoogleTranslation extends Command
             $responseCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);      //Here we fetch the HTTP response code
             curl_close($handle);
 
-            if($responseCode == 200) {
+            if ($responseCode == 200) {
                 $translatedText = $responseDecoded['data']['translations'][0]['translatedText'];
 
                 $transaction_data = [
@@ -140,13 +166,13 @@ class GoogleTranslation extends Command
                 $user->update($transaction_data);
 
                 // Store Account Id
-                if($column == 'full_name' && $language_allowed == 'en'){
-                    $user->account_id = Str::slug(substr($translatedText, 0, 4), "_").'_'.time();
+                if ($column == 'full_name' && $language_allowed == 'en') {
+                    $user->account_id = Str::slug(substr($translatedText, 0, 4), "_") . '_' . time();
                 }
 
                 $user->save();
-                
-                $message = 'User Id : '.$user->id.' details translated successfully !!!';
+
+                $message = 'User Id : ' . $user->id . ' details translated successfully !!!';
             }
         }
 
