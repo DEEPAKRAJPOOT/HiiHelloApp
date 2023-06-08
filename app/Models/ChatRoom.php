0cf699ce0b1c56115365e051d16c3e37ae7b110c
Rohan Garg <rohan.garg@saturdaytechnologies.io>
2023-06-08 18:01:16 +0530

    FIXED: Latest Message if expired

--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -18,7 +18,10 @@ class ChatRoom extends Model
     public function creator(){ return $this->belongsTo('App\Models\User','creator_id','id'); }
     public function participator(){ return $this->belongsTo('App\Models\User','participate_id','id'); }
     public function blockBy(){ return $this->belongsTo('App\Models\User','block_by','id'); }
-    public function latestMessage() { return $this->hasOne(ChatMessage::class,'room_id','id')->latest('id'); }
+    public function latestMessage() { return $this->hasOne(ChatMessage::class,'room_id','id')->where(function($query){
+        $query->whereNull('expired_at');
+        $query->orWhere('expired_at','>',now());
+    })->latest('id'); }
     public function chatMessages() { return $this->hasMany(ChatMessage::class,'room_id','id'); }
     public function callLog() { return $this->hasOne(CallLog::class,'room_id','id')->latest(); }
 
