cb57437417e2a2469860561e3367b4040fc92701
Rohan Garg <rohan.garg@saturdaytechnologies.io>
2023-06-21 16:44:44 +0530

    CHANGED: Admin Update wihtout overlapping

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -116,7 +116,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             $scheculeCommand = new AdminDashboard;
             $scheculeCommand->handle();
-        })->everyFifteenMinutes();
+        })->name('AdminDashboardUpdate')->everyFifteenMinutes()->withoutOverlapping();
         
 
         // Calculate Trust Scroe on the first day of every month at 2:00
