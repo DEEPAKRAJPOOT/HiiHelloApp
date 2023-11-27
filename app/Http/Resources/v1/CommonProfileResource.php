eb92295baa1594629eda91c88b45b203ac280ce7
Deepak Singh <techiesdeepak.singh@gmail.com>
2023-11-27 14:38:15 +0530

    like api gender modified

--- app/Http/Resources/v1/CommonProfileResource.php
+++ app/Http/Resources/v1/CommonProfileResource.php
@@ -20,7 +20,7 @@ class CommonProfileResource extends JsonResource
             'age'               =>  $this->getAge(),
             'location'          =>  new LocationResource($this->location),
             'profile_photo'     =>  generateURL($this->profile_photo) ?? "",
-            'Gender'            =>  $this->gender,
+            'gender'            =>  $this->gender,
             'isProfileVerified' => ($this->emailVerifyStatus()=='verified' && $this->contactVerifyStatus()=='verified' && $this->verify_photo_status=='verified') ? true : false,
         ];
         return parent::toArray($request);
