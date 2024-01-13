af8cac31628bc8969ea2f72eb046d6f94a0d0ba0
Deepak Singh <techiesdeepak.singh@gmail.com>
2024-01-13 11:09:12 +0530

    status modified with false

--- app/Http/Controllers/api/v1/GeneralController.php
+++ app/Http/Controllers/api/v1/GeneralController.php
@@ -92,7 +92,7 @@ class GeneralController extends Controller
             ],
             'phone_auth_type' => [
                 'android' =>[
-                    'firebase_sms_enabled' => true,
+                    'firebase_sms_enabled' => false,
                     'whatsapp_login_enabled'=>true
                 ]
             ],
