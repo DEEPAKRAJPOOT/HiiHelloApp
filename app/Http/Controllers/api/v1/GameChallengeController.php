89b8f91fa34daf160968fc54294fc6ea3ccd8552
Deepak Singh <techiesdeepak.singh@gmail.com>
2024-04-05 19:23:03 +0530

    #rechange

--- app/Http/Controllers/api/v1/GameChallengeController.php
+++ app/Http/Controllers/api/v1/GameChallengeController.php
@@ -640,7 +640,7 @@ public function generatePayload($user_id, $challenger_id=null, $type){
                         }else{
                             $receiver_id = $challenger_id;
                             $senderDeviceToken = DeviceToken::where(['user_id'=>$challenger_id])->orderBy('id','desc')->first();
-                            $payloadData = $senderDeviceToken;
+                            $payloadData = $receiverChallengeData;
                         }
                         
                         if($senderDeviceToken != null){
@@ -666,6 +666,7 @@ public function generatePayload($user_id, $challenger_id=null, $type){
                             
                             
                         }
+                        // dd($notifyData);
                         return $notifyData;
                     }else{
 
@@ -721,7 +722,7 @@ public function generatePayload($user_id, $challenger_id=null, $type){
                 } 
         }
         
-        return $challengeData;
+       return $challengeData;
     }
 
     public function isReadToPlay($onlineStatus,$status){
