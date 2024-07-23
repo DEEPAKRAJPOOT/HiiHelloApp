fd5172d913a64797d6fcd1dce6f72877cf3e3488
Deepak Singh <techiesdeepak.singh@gmail.com>
2024-07-23 14:20:56 +0530

    #rechange

--- app/Http/Resources/v1/HomeResource.php
+++ app/Http/Resources/v1/HomeResource.php
@@ -25,15 +25,17 @@ class HomeResource extends JsonResource
             'is_contact_verify' =>  ($this->contactVerifyStatus()=='verified') ? true : false,
             'is_photo_verify'   =>  ($this->verify_photo_status=='verified') ? true : false,
             'trusted_score'     =>  $this->trusted_score,
+            'relationship_status_id'=> (isset($this->relationship_status_id))?$this->relationship_status_id:null,
+            'education_id'      => (isset($this->education_id))?$this->education_id:null,
             'location'          =>  new LocationResource($this->location),
-            'interests'         =>  HomeInterestResource::collection($this->interests),
+            'interests'         =>  (!empty($this->interests))?HomeInterestResource::collection($this->interests):[],
             'profile_photo'     =>  generateURL($this->profile_photo) ?? "",           
             'media' =>  [
                 'profile_images'    =>  $this->getProfileImages(),
                 'profile_videos'    =>  $this->getProfileVideos(),
                 'profile_voice'     =>  [
-                    'voice'         =>  generateURL($this->voice),
-                    'voice_answer'  =>  $this->voice_answer ?? "",
+                    'voice'         =>  (isset($this->voice))?generateURL($this->voice):null,
+                    'voice_answer'  =>  (isset($this->voice_answer))?generateURL($this->voice_answer):null ?? "",
                 ],
             ],
             'flags'             =>  [
