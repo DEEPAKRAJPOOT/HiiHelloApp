5ac9008dafdbdff5e504c49a3c7cdd7fe5430348
Deepak Singh <techiesdeepak.singh@gmail.com>
2024-07-26 12:32:33 +0530

    #rechange

--- app/Http/Resources/v2/ChatRoomResourceMongoose.php
+++ app/Http/Resources/v2/ChatRoomResourceMongoose.php
@@ -25,7 +25,7 @@ class ChatRoomResourceMongoose extends JsonResource
         $roomId = $this->_id instanceof ObjectId ? $this->_id : new ObjectId($this->_id);
 
         $authLatestMessage = null;
-
+        dd($this->participate_cleared_at);
         if ($this->_id == config('utility.chat.system_chat_room')) {
             $authLatestMessage = ChatMessageMongoose::select('custom_id', 'status', 'created_at', 'updated_at', 'deleted_at', 'expired_at', 'is_vanished', 'message','sender_id','created_on', 'updated_on')
                 ->with(['sender' => function ($query) {
@@ -43,6 +43,7 @@ class ChatRoomResourceMongoose extends JsonResource
                 }])
                 ->withTrashed()
                 ->where('room_id', $roomId)
+                ->where('created_at','>',$this->participate_cleared_at ?? '')
                 ->where(function ($query) {
                     $query->where('is_vanished', false);
                     $query->orWhere('status', '!=', 'read');
@@ -60,6 +61,7 @@ class ChatRoomResourceMongoose extends JsonResource
                 }])
                 ->withTrashed()
                 ->where('room_id', $roomId)
+                ->where('created_at','>',$this->participate_cleared_at ?? '')
                 ->where(function ($query) {
                     $query->where('is_vanished', false);
                     $query->orWhere('status', '!=', 'read');
